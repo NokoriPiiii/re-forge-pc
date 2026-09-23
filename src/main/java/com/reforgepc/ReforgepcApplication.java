3312056403915955373bf05f1122bdb0cf90cac3
NokoriPiiii <huy171205@gmail.com>
2026-09-23 18:55:33 +0900

    scaffold

---
+++ src/main/java/com/reforgepc/ReforgepcApplication.java
@@ -0,0 +1,13 @@
+package com.reforgepc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReforgepcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReforgepcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reforgepc/ReforgepcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reforgepc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReforgepcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
